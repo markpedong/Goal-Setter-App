e4e51451adbd3d00d966a8a4b38880823407dff5
Omega <Omega@wf.local>
2022-06-09 16:15:06 +0800

    created a registration page for the project

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Dashboard from "./Pages/Dashboard";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useState, useEffect, FormEventHandler } from "react";
+import { FaUser } from "react-icons/fa";
 
-type Props = {};
+const Register = () => {
+  //create a formData state
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-const Register = (props: Props) => {
-  return <div>Register</div>;
+  const { name, email, password, password2 } = formData;
+
+  const onChange = (e: React.ChangeEvent) => {};
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className="form">
+        <form>
+          <div className="form-group">
+            <input
+              type="text"
+              id="name"
+              value={name}
+              placeholder="Enter your name"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              id="email"
+              value={email}
+              placeholder="Enter your email"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              id="password"
+              value={password}
+              placeholder="Enter your password"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              id="password"
+              value={password2}
+              placeholder="Enter your password again"
+              className="form-control"
+              onChange={onChange}
+            />
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Register;
